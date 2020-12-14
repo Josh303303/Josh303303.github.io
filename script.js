4a95373c841af413acddf0bbe8214a76cf8ca815
Your Name <you@example.com>
2020-12-14 16:09:56 +0000

    Added styling and scripting to navbar for homepage

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-script
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
